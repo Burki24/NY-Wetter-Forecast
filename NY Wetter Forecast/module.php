ca5ce95c59e663e7169bcd3e5066a0b7d903740b
Burki24 <burkhard@kneiseler.de>
2021-03-22 10:14:48 +0100

    Grundlagen in Form.json und Module.php geschaffen

--- NY Wetter Forecast/module.php
+++ NY Wetter Forecast/module.php
@@ -1,12 +1,19 @@
 <?php
 
 declare(strict_types=1);
-	class NYWetterForecast extends IPSModule
+	class NY_Wetter extends IPSModule
 	{
 		public function Create()
 		{
 			//Never delete this line!
 			parent::Create();
+
+			$this->RequireParent('{4CB91589-CE01-4700-906F-26320EFCF6C4}');
+
+
+			$this->RegisterPropertyString('Longitude', '');
+			$this->RegisterPropertyString('Latitude', '');
+			$this->RegisterPropertyString('Name', '');
 		}
 
 		public function Destroy()
@@ -19,5 +26,33 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
+
+			$lot = $this->ReadPropertyString('Longitude');
+			$lat = $this->ReadPropertyString('Latitude');
+			$nam = $this->ReadPropertyString('Name');
+		
+					}
+
+		public function Send(string $RequestMethod, string $RequestURL, string $RequestData, int $Timeout)
+		{
+		}
+
+		public function ReceiveData($JSONString)
+		{
+				// create curl resource
+			$ch = curl_init();
+
+		// url setzen
+			curl_setopt($ch, CURLOPT_URL, "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=$lat&lon=$lot");
+
+		// daten als String setzen und Browser vorgeben
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+			$JSONString = curl_exec($ch); // $JSONString beinhaltet den String
+
+			curl_close($ch); // curl schliessen zwecks Speicher-Entlastung
+			$data = json_decode($JSONString);  // String in array wandeln
+			
 		}
 	}
